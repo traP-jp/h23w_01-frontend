d521ce416df345bbf98a6f7d7396b4d52ea1a67f
mehm8128 <mehm8128@gmail.com>
2023-12-14 22:23:53 +0900

    レイアウト適当に整えた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,19 @@ import { PostForm } from '@/features/form/Form'
 
 export default function Home() {
 	return (
-		<main>
-			<ColorSelector />
-			<ObjectSelector />
-			<OtherSelector />
-			<History />
-			<FabricCanvasWrapper />
-			<PostForm />
+		<main className="flex gap-12 pt-8 px-10">
+			<div className="space-y-8 flex-1">
+				<ColorSelector />
+				<ObjectSelector />
+				<OtherSelector />
+			</div>
+			<div>
+				<FabricCanvasWrapper />
+			</div>
+			<div className="flex flex-col justify-between flex-1">
+				<History />
+				<PostForm />
+			</div>
 		</main>
 	)
 }
