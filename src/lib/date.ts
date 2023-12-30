3c5c4cfd2532de85650b7375601ac8641ace7f0a
mehm8128 <mehm8128@gmail.com>
2023-12-30 21:48:47 +0900

    時差を適当に足した

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,5 @@
 export const datetimeToString = (date: Date): string => {
+	date.setHours(date.getHours() + 9)
 	return `${date.getFullYear()}/${(date.getMonth() + 1)
 		.toString()
 		.padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')} ${date
@@ -8,6 +9,7 @@ export const datetimeToString = (date: Date): string => {
 }
 
 export const timeToString = (date: Date): string => {
+	date.setHours(date.getHours() + 9)
 	return `${date.getHours().toString().padStart(2, '0')}:${date
 		.getMinutes()
 		.toString()
